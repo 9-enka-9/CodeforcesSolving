5f7c96ecd80664b86d112bd5c22f97dace660e4c
9-enka-9 <minhen520tp@gmail.com>
2024-09-07 16:17:22 +0700

    bai4

---
+++ OLP 11 2023/bai4.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s;
+	cin>>s;
+	int len=s.length(), reslen=len-1;
+	for (int i=0; i<len; i++){
+		int c=1;
+		for (int start=1;start<=reslen; start++){
+			bool ok=true;
+			for (int j=0; j<=i; j++){
+//				cout<<j+start<<" "<<s[j+start]<<" and "<<j<<" "<<s[j]<<endl;
+				if (s[start+j] != s[j]){
+					ok=false;
+					break;
+				}
+			}
+			c+=ok;
+		}
+		cout<<c<<" ";
+		reslen--;
+		if (c==1){
+			break;
+		}
+	}
+	for (int i=0;i<=reslen;i++){
+		cout<<1<<" ";
+	}
+}
